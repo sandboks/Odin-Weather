2b8c3e7387257122faabb784bc528c8905108548
Sand <sandboxasdf@gmail.com>
2025-01-22 20:44:24 +1100

    fun with timezones

--- src/weather.js
+++ src/weather.js
@@ -11,13 +11,30 @@ export const WeatherData = (function () {
     }
 
     async function GetWeatherDataFromLocation(location) {
-        //let string = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tokyo?unitGroup=metric&include=days&key=VLJ2ZNYEW4LQATMDBET324BMR&contentType=json";
         let request = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days&key=VLJ2ZNYEW4LQATMDBET324BMR&contentType=json`;
-        //let data = null;
 
         const response = await fetch(request, {mode: 'cors'});
         const data = await response.json();
+
+        GetCurrentTimeInTimezone(data.tzoffset);
+
+
+
         console.log(data);
+    }
+
+    function GetCurrentTimeInTimezone(timezone) {
+    // create Date object for current location
+    var date = new Date();
+
+    // convert to milliseconds, add local time zone offset and get UTC time in milliseconds
+    var utcTime = date.getTime() + (date.getTimezoneOffset() * 60000);
+
+    // create new Date object for a different timezone using supplied its GMT offset.
+    var currentTime = new Date(utcTime + (3600000 * timezone));
+
+    currentTime.getTimezoneOffset();
+    console.log(`The time in this place is: ${currentTime.toLocaleTimeString()}`);
     }
 
     return {
